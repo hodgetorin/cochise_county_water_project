81d9e86e73ac15cdac6f4a236461185fdb80bda9
Torin <74333325+hodgetorin@users.noreply.github.com>
2021-04-02 21:37:46 -0700

    Update Water_Project_Apr2.py

--- Water_Project_Apr2.py
+++ Water_Project_Apr2.py
@@ -29,10 +29,10 @@ df.dropna(subset=['drilldate'], inplace=True)
 df['year']=df['drilldate'].dt.year
 
 # new dataframe of only observations since 1980
-df1970 = df[(df['year'] >= 1980)]
+df1980 = df[(df['year'] >= 1980)]
 
-print(len(df1970))
-print(df1970.head(5))
+print(len(df1980))
+print(df1980.head(5))
 
 
 # In[5]:
@@ -42,7 +42,7 @@ df.dtypes
 
 fig, ax = plt.subplots(figsize=(25,25))
 
-sns.boxplot(x=df1970['year'], y=df1970['depth'], data=df, whis=[0,100])
+sns.boxplot(x=df1980['year'], y=df1980['depth'], data=df, whis=[0,100])
 ax.set_title("Well Depths vs. Year in Sunsites, AZ 1980-2021")
 ax.set_ylabel("Well Depth (ft)")
 ax.set_xlabel("Year")
@@ -52,10 +52,3 @@ plt.xticks(rotation=45)
 sns.despine(trim=True, left=True)
 
 plt.show()
-
-
-# In[ ]:
-
-
-
-
